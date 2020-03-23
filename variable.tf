51d6f37581e71f954f9ad03b81207bfff46c7bad
sushmitabhandari <42686549+sushmitabhandari@users.noreply.github.com>
2020-03-23 15:52:53 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -5,7 +5,7 @@
 #####\_/_/   \_\_| \_\___/_/   \_\____/|_____|_____|
 
 variable "region" {
-  default     = "us-east-1"
+  default     = "ap-south-1"
   description = "Region"
 }
 
@@ -33,13 +33,13 @@ variable "nodes_defaults" {
   default = {
     name                 = "eks-nodes"    # Name for the eks workers.
     ami_id               = "ami-dea4d5a1" # AMI ID for the eks workers. If none is provided, Terraform will searchfor the latest version of their EKS optimized worker AMI.
-    asg_desired_capacity = "2"            # Desired worker capacity in the autoscaling group.
-    asg_max_size         = "3"            # Maximum worker capacity in the autoscaling group.
-    asg_min_size         = "2"            # Minimum worker capacity in the autoscaling group.
-    instance_type        = "t2.small"     # Size of the workers instances.
-    key_name             = "eks-key"      # The key name that should be used for the instances in the autoscaling group
+    asg_desired_capacity = "1"            # Desired worker capacity in the autoscaling group.
+    asg_max_size         = "1"            # Maximum worker capacity in the autoscaling group.
+    asg_min_size         = "1"            # Minimum worker capacity in the autoscaling group.
+    instance_type        = "t2.micro"     # Size of the workers instances.
+    key_name             = "gocorona"      # The key name that should be used for the instances in the autoscaling group
     ebs_optimized        = false          # sets whether to use ebs optimization on supported types.
-    public_ip            = false          # Associate a public ip address with a worker
+    public_ip            = true          # Associate a public ip address with a worker
   }
 }
 
