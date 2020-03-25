ac004d28e0c5c96d864a5a3eb107197492d5c73f
naveen-19 <55230320+naveen-19@users.noreply.github.com>
2020-03-25 12:03:42 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,5 +1,5 @@
 ##__     ___    ____  ___    _    ____  _     _____
-##\ \   / / \  |  _ \|_ _|  / \  | __ )| |   | ____|
+##\ \   / / \  |  _ \|_ _|  /   | __ )| |   | ____|
 ###\ \ / / _ \ | |_) || |  / _ \ |  _ \| |   |  _|
 ####\ V / ___ \|  _ < | | / ___ \| |_) | |___| |___
 #####\_/_/   \_\_| \_\___/_/   \_\____/|_____|_____|
@@ -23,7 +23,7 @@ variable "env" {
 }
 
 variable "external-ip" {
-  default = "5.194.139.128/32"
+  default = "192.168.1.101/32"
 }
 
 variable "nodes_defaults" {
@@ -32,12 +32,12 @@ variable "nodes_defaults" {
 
   default = {
     name                 = "eks-nodes"    # Name for the eks workers.
-    ami_id               = "ami-dea4d5a1" # AMI ID for the eks workers. If none is provided, Terraform will searchfor the latest version of their EKS optimized worker AMI.
+    ami_id               = "ami-03b5297d565ef30a6" # AMI ID for the eks workers. If none is provided, Terraform will searchfor the latest version of their EKS optimized worker AMI.
     asg_desired_capacity = "1"            # Desired worker capacity in the autoscaling group.
     asg_max_size         = "1"            # Maximum worker capacity in the autoscaling group.
     asg_min_size         = "1"            # Minimum worker capacity in the autoscaling group.
     instance_type        = "t2.micro"     # Size of the workers instances.
-    key_name             = "gocorona"      # The key name that should be used for the instances in the autoscaling group
+    key_name             = "dev"      # The key name that should be used for the instances in the autoscaling group
     ebs_optimized        = false          # sets whether to use ebs optimization on supported types.
     public_ip            = true          # Associate a public ip address with a worker
   }
